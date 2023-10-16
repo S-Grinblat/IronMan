8ec51a60d40892df9512533a9d7539529dbfb51e
Santiago <105889534+S-Grinblat@users.noreply.github.com>
2023-10-16 19:00:15 -0300

    'commit final 16-10-2023'

--- src/components/InteractiveAreas/AllSuits.jsx
+++ src/components/InteractiveAreas/AllSuits.jsx
@@ -4,9 +4,11 @@ const AllSuits = ({ Suits }) => {
   return (
     <div className='grid justify-center items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {Suits.map((Suit, index) => (
-        <div key={Suit.id} className='py-3 px-2 w-[320px] hover:shadow-[#ccc] shadow-inner transition-all rounded-xl'>
-          <img src={Suit.imagen} alt={Suit.nombre} className='img'/>
-          <p className='text-gray-600 text-center'>{Suit.descripcion}</p> 
+        <div key={Suit.id} className='py-3 px-2 w-[320px] hover:shadow-[#ccc] shadow-inner transition-all rounded-xl relative group'>
+          <img src={Suit.imagen} alt={Suit.nombre} className='img w-full h-auto group-hover:blur-md transition duration-300'/>
+          <div class="absolute inset-0 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition duration-300 flex justify-center items-center">
+            <p className='text-center px-3'>{Suit.descripcion}</p> 
+          </div>
         </div>
       ))}
     </div>
