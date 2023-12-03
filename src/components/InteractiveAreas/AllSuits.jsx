2e76d353f14463e6e24bf6e64571bd6afcde4476
Santiago <105889534+S-Grinblat@users.noreply.github.com>
2023-12-03 02:52:31 -0300

    'se realizaron mejoras de cargas y reenderizado'

--- src/components/InteractiveAreas/AllSuits.jsx
+++ src/components/InteractiveAreas/AllSuits.jsx
@@ -5,7 +5,7 @@ const AllSuits = ({ Suits }) => {
     <div className='grid justify-center items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {Suits.map((Suit, index) => (
         <div key={Suit.id} className='py-3 px-2 w-[320px] hover:shadow-[#ccc] shadow-inner transition-all rounded-xl relative group'>
-          <img src={Suit.imagen} alt={Suit.nombre} className='img w-full h-auto group-hover:blur-md transition duration-300'/>
+          <img src={Suit.imagen} loading="lazy" alt={Suit.nombre} className='img w-full h-auto group-hover:blur-md transition duration-300'/>
           <div class="absolute inset-0 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition duration-300 flex justify-center items-center">
             <p className='text-center px-3'>{Suit.descripcion}</p> 
           </div>
